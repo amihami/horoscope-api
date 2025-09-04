77f5f5b1f660bd63aa42c7391a668dd25ce1f9f9
amihami <hamiltonamel@gmail.com>
2025-09-04 17:02:11 +0100

    Horoscope project initialisation

---
+++ src/test/java/com/cbfacademy/horoscopeapi/HoroscopeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.horoscopeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroscopeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
